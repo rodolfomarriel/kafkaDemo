44d025d225b51633f0489d5fc29c754c6765c803
Rodolfo Marriel <rodolfo.29r@gmail.com>
2022-04-04 22:07:44 -0300

    "first commit"

---
+++ README.md
@@ -0,0 +1,8 @@
+#Kafka mini Project
+
+Producer and Consumer in the same application
+
+###URL Request
+````curl --location --request GET 'http://localhost:8080/kafka/send'````
+
+Project based on <a src="https://mmarcosab.medium.com/criando-consumer-e-produkafka-com-spring-boot-b427cc2f841d">this</a>
---
+++ src/main/java/com/example/kafka/kafkaDemo/KafkaDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.kafka.kafkaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@EnableKafka
+@SpringBootApplication
+public class KafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDemoApplication.class, args);
+	}
+
+}
